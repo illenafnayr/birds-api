fa7ceca37771e772ff2f89ce4e2921d87b3e58ab
illenafnayr <illenafnayr@gmail.com>
2023-09-26 16:30:36 +0200

    Removing target from git history

--- src/main/java/com/birds/api/model/User.java
+++ src/main/java/com/birds/api/model/User.java
@@ -1,9 +1,17 @@
 package com.birds.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer userId;
     private String username;
     private String password;
     private String firstName;
